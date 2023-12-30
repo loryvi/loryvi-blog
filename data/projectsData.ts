affe5c914ece9eafa452dfe93f970910dd6a96b0
loryvi <lilyleogo@gmail.com>
2023-12-30 23:06:32 +0800

    blog template

---
+++ data/projectsData.ts
@@ -0,0 +1,26 @@
+const projectsData = [
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '/blog/the-time-machine',
+  },
+  { title: 'Kamatis | Pomodoro Timer App',
+    description: `A 25 timer minutes for your pomodoro technique. It is a simple timer that will help you focus on your task.
+    Built with Python and Tkinter.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'blog/creating-order-component',
+  },
+]
+
+export default projectsData
